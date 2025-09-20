4baed570cae1f2a2457f2a6e4f412245ba07d2c1
langpeu <icopy@naver.com>
2025-09-20 12:27:46 +0900

    0. 리소스 추가
    - https://www.youtube.com/watch?v=jwN0o8xUdXk

--- iOS26Test/ContentView.swift
+++ iOS26Test/ContentView.swift
@@ -8,17 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isExpanded: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        /// Background Image
+        Image("Pic")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 300, height: 300)
+            .clipShape(.rect(cornerRadius: 20))
+            .overlay(alignment: .bottom) {
+                GlassEffectContainer(spacing: 20) {
+                    VStack(spacing: 20) {
+                        Spacer()
+                        
+                        if isExpanded {
+                            Group {
+                                Image(systemName: "suit.heart.fill")
+                                    .font(.title)
+                                    .foregroundStyle(.red.gradient)
+                                    .frame(width: 50, height: 50)
+                                
+                                Image(systemName: "magnifyingglass")
+                                    .font(.title)
+                                    .foregroundStyle(.white.gradient)
+                                    .frame(width: 50, height: 50)
+                            }
+                            .glassEffect(.regular, in: .circle)
+                            .glassEffectTransition(.identity)
+                        }
+                        
+                        Button {
+                            withAnimation(.smooth(duration: 1, extraBounce: 0)) {
+                                isExpanded.toggle()
+                            }
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .font(.title)
+                                .foregroundStyle(.white.gradient)
+                                .frame(width: 40, height: 40)
+                        }
+                        .buttonStyle(.glass)
+                    }
+                }
+                .padding(15)
+            }
     }
-}
-
-#Preview {
-    ContentView()
 }
