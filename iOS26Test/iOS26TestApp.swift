aa6fbd33b4e446e937578482bd87fc7ba79cda0d
langpeu <icopy@naver.com>
2025-09-20 12:01:18 +0900

    Initial Commit

---
+++ iOS26Test/iOS26TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS26TestApp.swift
+//  iOS26Test
+//
+//  Created by Langpeu on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS26TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS26TestTests/iOS26TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS26TestTests.swift
+//  iOS26TestTests
+//
+//  Created by Langpeu on 9/20/25.
+//
+
+import Testing
+@testable import iOS26Test
+
+struct iOS26TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
